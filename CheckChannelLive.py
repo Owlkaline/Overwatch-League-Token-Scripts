78e8359b8480fc4e91004062d17e04f4800b2c45
Lilith645 <lilith@inet-sys.com>
2021-06-16 10:50:02 +1000

    Now checks if the contenders website has actually started rewards Changes committed: 	modified:   CheckChannelLive.py

--- CheckChannelLive.py
+++ CheckChannelLive.py
@@ -16,6 +16,11 @@ import runpy
 urls = [['OWL', 'https://www.youtube.com/channel/UCiAInBL9kUzz1XRxk66v-gw/live'],
         ['Contenders', 'https://www.youtube.com/channel/UCWPW0pjx6gncOEnTW8kYzrg/live']]
 
+def report_not_live(settings, url):
+  settings.log(url + " is not live.");
+  if exists(settings.home_dir() + "/" + url_live):
+    os.remove(settings.home_dir() + "/" + url_live)
+
 settings = owl_settings.Settings();
 
 settings.log("Checking if ow channels are live.");
@@ -32,20 +37,35 @@ for url in urls:
   rewards_enabled = False;
   waiting_for_stream = False;
   
+  contenders_vid_started = False;
+  
   try:
     #rewards = driver.find_element_by_xpath('/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[6]/div[2]/ytd-video-primary-info-renderer/div/div/div[3]/div/ytd-menu-renderer/div[2]/ytd-account-link-button-renderer/div/ytd-button-renderer/a');
     stream_waiting = driver.find_element_by_xpath('/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[6]/div[2]/ytd-video-primary-info-renderer/div/div/div[1]/div[1]/ytd-video-view-count-renderer/span[1]').text;
-    
     #rewards_enabled = rewards.text == "REWARDS";
     
     waiting_for_stream = 'waiting' in stream_waiting;
     
   except selenium.common.exceptions.NoSuchElementException:
     live = False;
-
+  
+  if url[0] == "Contenders":
+    driver.get("https://overwatchleague.com/en-us/contenders");
+    time.sleep(20);
+    try:
+     contenders_rewards = driver.find_element_by_xpath("/html/body/div/div/div/div[4]/div[1]/div[3]/div/div/div/p").text;
+     print(contenders_rewards)
+     contenders_vid_started = "CONTENDERS VIEWERSHIP REWARDS" in contenders_rewards;
+    except:
+      contenders_vid_started = False;
+  
   driver.quit();
 
   if live and not waiting_for_stream: #and rewards_enabled and not waiting_for_stream:
+    if url[0] == "Contenders" and not contenders_vid_started:
+      report_not_live(settings, url[0]);
+      continue;
+    
     settings.log(url[0] + " is live!");
     if exists(settings.home_dir() + "/" + url_live):
       continue;
@@ -72,6 +92,4 @@ for url in urls:
     else:
       runpy.run_path(path_name=settings.home_dir() + "/.owl/open_contenders.py");
   else:
-    settings.log(url[0] + " is not live.");
-    if exists(settings.home_dir() + "/" + url_live):
-      os.remove(settings.home_dir() + "/" + url_live)
+    report_not_live(settings, url[0]);
